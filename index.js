1f04a15f88ecad6d82bc05f88385f0187798d4e6
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-11 15:49:28 -0300

    Feat: implementa a rota de login no index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routerTalker = require('./src/route/talkers');
+const routerLogin = require('./src/router/login');
 
 const app = express();
 app.use(bodyParser.json());
 
 const PORT = '3000';
 
+app.use('/login', routerLogin);
 app.use('/talker', routerTalker);
 
 app.listen(PORT, () => {
